a0f764bc55ec2a5d7fc461d3e1ad169eb7fa45ab
LukeDiB <141517474+LukeDiB@users.noreply.github.com>
2024-05-07 10:13:24 +0100

    Completed task 4

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+      <div>
+        <h1>NC-NEWS</h1>
+        <h2>The only place to get your news</h2>
+        <button>Home</button>
+        <button>Coding</button>
+        <button>Cooking</button>
+        <button>Football</button>
+      </div>
+    );
+}
+
+export default Header
