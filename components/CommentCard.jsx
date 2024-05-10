baf440f0a27537ba7a6e358eba76305e236837f0
LukeDiB <141517474+LukeDiB@users.noreply.github.com>
2024-05-10 12:22:32 +0100

    Refactoring delete comment

--- components/CommentCard.jsx
+++ components/CommentCard.jsx
@@ -5,32 +5,38 @@ function CommentCard(props) {
   const { comment } = props;
   const article_id = comment.article_id;
   const comment_id = comment.comment_id;
+  const [style, setStyle] = useState({})
   const [deleted, setDeleted] = useState("");
-  const [invalidUser, setInvalidUser] = useState('')
   const [user, setUser] = useState("grumpy19");
+  
 
   function handleDelete() {
-    if(comment.author !== user){
-      setInvalidUser('You must be the creator of the comment to delete it!')
-    } else{
     setDeleted("Deleted!");
-      deleteComment(article_id, comment_id);
-    }
+    deleteComment(article_id, comment_id);
   }
 
-    useEffect(() => {
-      setDeleted("");
-    }, [comment])
+  useEffect(() => {
+    if(user !== comment.author) {
+      setStyle({display:'none'})
+    }
+  }, [comment])
+
   return (
     <li className="no-bullet">
       <p>{deleted}</p>
-      <p id='invalid-user'>{invalidUser}</p>
       <h6>{comment.author}</h6>
       <h5>{comment.body}</h5>
       <div id="comment-votes">
         <p id="vote-count">{comment.votes}</p>
-        <button disabled={deleted === 'Deleted!'}onClick={handleDelete}>Delete</button>
-        <div></div>
+        <div>
+          <button
+            id="delete-button"
+            style={style}
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </li>
   );
